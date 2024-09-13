6e01a5889dc59c368c6195c2bf5dbf303c2c68e2
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-09-14 00:27:03 +0700

    Adding toast message sign out users

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,6 +7,8 @@ import { app } from "@/src/config/FirebaseConfig";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import ExitIcon from "./icons/ExitIcon";
+import { Avatar } from "@nextui-org/react";
+import UserIcon from "./icons/UserIcon";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -31,10 +33,39 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
   const handleLogout = async () => {
     try {
-      toast.success("Sampai jumpa lagi!", {
-        duration: 3000,
-      });
       router.push("/");
+      toast.custom(
+        (t) => (
+          <>
+            <div className="max-w-xs relative bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert" aria-labelledby="hs-toast-avatar-label">
+              <div className="flex p-4">
+                <div className="shrink-0">
+                  {user && user.photoURL ? <Avatar src={user.photoURL} className="size-8 text-large" /> : <UserIcon className="w-8 h-8" />}
+                  <button
+                    type="button"
+                    onClick={() => toast.dismiss(t.id)}
+                    className="absolute top-3 end-3 inline-flex shrink-0 justify-center items-center size-5 rounded-lg text-gray-800 opacity-50 hover:opacity-100 focus:outline-none focus:opacity-100 dark:text-white"
+                    aria-label="Close">
+                    <span className="sr-only">Close</span>
+                    <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <path d="M18 6 6 18"></path>
+                      <path d="m6 6 12 12"></path>
+                    </svg>
+                  </button>
+                </div>
+                <div className="ms-4 me-5">
+                  <h3 id="hs-toast-avatar-label" className="text-gray-800 font-medium text-sm dark:text-white">
+                    <span className="font-semibold">{user.displayName}</span>, Sampai Jumpa Kembali 👋
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </>
+        ),
+        {
+          duration: 6000,
+        }
+      );
       await signOut(auth);
     } catch (error: any) {
       console.log("Error signing out: ", error.message);
